fc3c389fec3abff81cd3cd40a1a66cb457678da6
User <serenareci1@gmail.com>
2024-03-27 19:38:37 +0100

    Security package filled

--- TravelAgency/src/main/java/com/sda/TravelAgency/security/MyUserDetails.java
+++ TravelAgency/src/main/java/com/sda/TravelAgency/security/MyUserDetails.java
@@ -1,4 +1,55 @@
 package com.sda.TravelAgency.security;
 
-public class MyUserDetails {
+
+import com.sda.TravelAgency.entity.Role;
+import com.sda.TravelAgency.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+public class MyUserDetails implements UserDetails {
+
+    private User userEntity;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> userRoles = userEntity.getRoles();
+        List<SimpleGrantedAuthority> listToAddAllTheAuthorizations = new ArrayList<>();
+        for (Role role : userRoles) {
+            listToAddAllTheAuthorizations.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+        return listToAddAllTheAuthorizations;
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+    @Override
+    public String getUsername() {
+        return userEntity.getEmail();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- TravelAgency/src/main/java/com/sda/TravelAgency/security/MyUserDetailsService.java
+++ TravelAgency/src/main/java/com/sda/TravelAgency/security/MyUserDetailsService.java
@@ -1,4 +1,31 @@
 package com.sda.TravelAgency.security;
 
-public class MyUserDetailsService {
+import com.sda.TravelAgency.entity.User;
+import com.sda.TravelAgency.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
+
+        Set<GrantedAuthority> authorities = userEntity.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toSet());
+        return new org.springframework.security.core.userdetails.User(userEntity.getUsername(), userEntity.getPassword(), authorities);
+
+    }
 }
